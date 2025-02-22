f4d87d86e267716362203124b1f1548aa9d90d84
Antonio Felix Glorioso Ceretti <antonioglorioso1@hotmail.com>
2025-02-22 14:07:40 -0300

    Agregado boton de volver y borrado boton de "Cambiar Perfil", arreglos sobre los formularios y pantalla de inicio del usuario.

--- app/src/main/java/com/example/juegosdidacticos_limpiezadecaballo/MainActivity.kt
+++ app/src/main/java/com/example/juegosdidacticos_limpiezadecaballo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.juegosdidacticos_limpiezadecaballo
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -17,12 +18,31 @@ class MainActivity : AppCompatActivity() {
         binding = MainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize NavController
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        if (intent.getBooleanExtra("SHOW_USER_INIT_FRAGMENT", false)) {
+        binding.backButton.setOnClickListener {
+            if (navController.currentDestination?.id == R.id.UserInitPage) {
+                navController.navigate(R.id.UserSelectionPage)
+            } else {
+                navController.navigateUp()
+            }
+        }
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.BeginPageFragment, R.id.UserSelectionPage -> {
+                    binding.backButton.visibility = View.GONE
+                }
+                else -> {
+                    binding.backButton.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        if (intent.getBooleanExtra("SHOW_USER_INIT_FRAGMENT", false)) {
             val selectedUser = intent.getParcelableExtra("USER_DATA", NamedEntity::class.java)
             val args = Bundle().apply {
                 putParcelable("selectedUser", selectedUser)
